68abf74cc4fb9cabcccb919b9bd838ed110ea0f7
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2024-03-30 10:49:52 +0600

    migrate backendend to typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,5 @@
+import { Application, Request, Response } from "express";
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -5,15 +7,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app = express();
+const app:Application = express();
 app.use(cors({
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
   res.json({ status: "success" });
 });
 
