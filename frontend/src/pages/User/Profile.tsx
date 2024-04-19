d5052b2014dbab3106a64b8df50f52f3e19dd508
Nahian Chowdhury <nahianchowdhury00777@gmail.com>
2024-04-20 01:56:08 +0600

    navbar design

--- frontend/src/pages/User/Profile.tsx
+++ frontend/src/pages/User/Profile.tsx
@@ -1,29 +1,26 @@
 import React from "react";
-import logo from "../../assets/logo.png";
-import menu_icon from "../../assets/menu.png";
-import search_icon from "../../assets/search.png";
-import notification_icon from "../../assets/notification.png";
-import profile_icon from "../../assets/user_profile.jpg";
-// import logout_icon from "../../assets/logout.png";
+import { FaSearch } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
 function Profile() {
-  return <div className=" bg-black">
-    hellow
-    {/* <nav className="p-4 w-full flex justify-between shadow-md bg-white sticky top-0 z-10">
-      <div className="nav-left flex justify-left items-start">
-        <img className="m-5 w-8" src={menu_icon} alt="menu_icon" />
-        <img className=" w-20" src={logo} alt="logo" />
+  return <div className="w-full">
+    <nav className="m-4 p-4 w-3/50 flex justify-between shadow-md bg-white sticky top-0 z-10 rounded-full">
+      <div className="nav-left flex justify-left items-center">
+        <IoMdMenu className="m-2 w-8 h-8"/>
+        {/* <img className=" w-20" src={logo} alt="logo" /> */}
       </div>
       <div className="nav-middle flex justify-center items-center">
-        <input className="search-box border border-black mr-2 pl-2 pr-3 rounded-full" type="text" placeholder="Search" />
-        <img src={search_icon} alt="" />
+        <input className="search-box border border-black mr-2 pl-2 pr-8 pt-1 pb-1 rounded-full text-center" type="text" placeholder="Search" />
+        <FaSearch className="w-6 h-6"/>
       </div>
-      <div className="nav-right flex justify-end items-end">
-        <img src={notification_icon} alt="" />
-        <img src={profile_icon} alt="" />
-        <img src={logout_icon} alt="" />
+      <div className="nav-right flex items-center">
+        <IoIosNotificationsOutline className="m-3 w-10 h-10" />
+        <CgProfile className="m-1 w-14 h-14" />
+        {/* <img src={logout_icon} alt="" /> */}
       </div>
-    </nav> */}
+    </nav>
   </div>;
 }
 
