6a26655f9a00ef55847ca4d29d63d31e01f1fc02
nahian00777 <nahianchowdhury00777@gmail.com>
2024-04-27 18:04:23 +0600

    Search Page in User

--- frontend/src/pages/User/Search.tsx
+++ frontend/src/pages/User/Search.tsx
@@ -3,7 +3,36 @@ import React from "react";
 function Search() {
   return (
     <>
-      <div className="">search</div>
+      <div className="">
+        <div className="bg-gradient-to-r from-blue-500 to-slate-300 text-transparent bg-clip-text m-4 p-4 flex justify-center items-center">
+          <p className=" font-bold p-2 m-2 text-7xl">FIND YOUR EXPERT</p>
+        </div>
+        <div className="flex justify-center items-center">
+          <ul className=" ml-32 mr-32 flex justify-center items-center flex-wrap">
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Explore</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Learning</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Health</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Desging System</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Mobile Design</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Desging System</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Desging System</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Desging System</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Desging System</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Mobile Design</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Mobile Design</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Mobile Design</li>
+            <li className="p-1 m-1 border-2 rounded-btn border-gray-600 text-gray-600">Mobile Design</li>
+          </ul>
+        </div>
+        <div className=" m-8">
+          <p className="text-4xl flex justify-center font-bold">Explore Experts Around The World</p>
+          <p className="text-2xl flex justify-center font-semibold">Explore, and choose the best advisor for you and Glorify Your Skill</p>
+        </div>
+        <div className="flex justify-left items-center border-2 border-gray-500 rounded-btn
+          h-10 my-10 mx-80 ">
+          <p className=" text-gray-500 mx-3 p-1">Search For Expert Advice</p>
+        </div>
+      </div>
     </>
   );
 }
